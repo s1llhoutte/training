85edb1221f478efd98f0ef12c9d3910431f9e3c2
s1llhoutte <danikval2000002@gmail.com>
2023-07-12 13:40:06 +0300

    peremenii

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo '123';
-echo '234';
+$name = 'Victor';
+$age = 20;
+$hobby = 'swimming';
+$isMarried = true;
+$pet = 'cat';
+$petName = 'Barsik';
+
+echo $name . "\n";
+echo $age . "\n";
+echo $hobby . "\n";
+echo $isMarried . "\n";
+echo $pet . "\n";
+echo $petName . "\n";
