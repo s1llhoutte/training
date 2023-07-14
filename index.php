a9e283c8f48501b87bb0e109163ba85c3bd45380
s1llhoutte <danikval2000002@gmail.com>
2023-07-14 12:14:29 +0300

    massive

--- index.php
+++ index.php
@@ -1,14 +1,49 @@
 <?php
-$name = 'Victor';
-$age = 20;
-$hobby = 'swimming';
-$isMarried = true;
-$pet = 'cat';
-$petName = 'Barsik';
+$person = [
+    'name' => 'Victor',
+    'age' => 20,
+    'hobby' => 'swimming',
+    'is_married' => true,
+    'pet' => 'cat',
+    'pet_name' => 'Barsik'
+];
 
-echo $name . "\n";
-echo $age . "\n";
-echo $hobby . "\n";
-echo $isMarried . "\n";
-echo $pet . "\n";
-echo $petName . "\n";
+print_r($person);
+
+//как добавить в массив еще одно данное
+$person1 = [
+    'name' => 'Vasya',
+    'age' => 20
+];
+
+$person1['hobby'] = 'cat';
+
+print_r($person1);
+//удалить значение в масиве
+unset($person1['name']);
+
+print_r($person1);
+
+
+
+
+// Домашка
+$person2 = [
+    'name' => 'Danil',
+    'age' => 21,
+    'hobby' => 'developer',
+    'is_married' => false,
+    'pet' => 'nothing',
+    'pet_name' => 'nothing'
+];
+
+$person3 = [
+    'name' => 'Valik',
+    'age' => 22,
+    'hobby' => 'swimming',
+    'is_married' => false,
+    'pet' => 'cat',
+    'pet_name' => 'Dasha'];
+
+print_r($person2);
+print_r($person3);
