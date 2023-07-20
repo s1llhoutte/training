3cfe98b714f852b823b188cc8b927831d0e78696
s1llhoutte <danikval2000002@gmail.com>
2023-07-20 13:34:33 +0300

    nasledovanie

--- index.php
+++ index.php
@@ -1,63 +1,75 @@
 <?php
+
 class Person
 {
     public $name = 'Bob';
     public $age = 20;
+    public $job = 'developer';
+
+    public function sayHello()
+    {
+        echo 'Hello, i\'m Bob';
+    }
+}
+
+class Developer extends Person
+{
+    public function work()
+    {
+        echo 'developer is working';
+    }
+}
+
+class Director extends Person
+{
+    public function work()
+    {
+        echo 'director is working';
+    }
+}
+
+class Manager extends Person
+{
+    public function work()
+    {
+        echo 'manager is working';
+    }
+}
+
+$manager = new Manager();
+echo $manager->name;
+
+$director = new Director();
+$director->sayHello();
+
+
+// Домашка
+
+class Persona
+{
+    public $name = 'Victor';
+    public $age = 34;
     public $hobby = 'swimming';
-    public $isMarried = false;
-
-    public function __construct($name, $age, $hobby, $isMarried)
-    {
-        $this->name = $name;
-        $this->age = $age;
-        $this->hobby = $hobby;
-        $this->isMarried = $isMarried;
-    }
-
 }
 
-$person = new Person('Danil', 20, 'gym', false);
-
-echo $person->name;
-
-
-
-// домашка
-class FirstTochka
+class Pojarnii extends Persona
 {
-    public $pomidor;
-    public $ogyrec;
-    public $ketchup;
-
-    public function __construct($pomidor, $ogyrec, $ketchup)
+    public function work()
     {
-        $this->pomidor = $pomidor;
-        $this->ogyrec = $ogyrec;
-        $this->ketchup = $ketchup;
+        echo 'Pojarnii is working';
     }
 }
 
-$firstTochka = new FirstTochka('rozovii','green', 'torchin');
+$pojarnii = new Pojarnii();
+echo $pojarnii->name;
 
-echo $firstTochka->ketchup;
-
-class SecondTochka
+class Electric extends Persona
 {
-    public $products = [
-      'ogyrec' => 'green',
-      'pomidor' => 'red',
-      'salat' => 'green',
-      'paprika' => 'red'
-    ];
-
-    public function __construct($ogyrec, $pomidor, $salat, $paprika)
+    public function work()
     {
-        $this->products['ogyrec'] = $ogyrec;
-        $this->products['pomidor'] = $pomidor;
-        $this->products['salat'] = $salat;
-        $this->products['paprika'] = $paprika;
+        echo 'Electric is also working';
     }
 }
-$secondTochka = new SecondTochka('47 grn', '38 grn', '10 grn', '5 grn');
 
-echo $secondTochka->products['ogyrec'];
+$electric = new Electric();
+echo $electric->age;
